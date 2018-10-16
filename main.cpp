c4201278fe4f98b00c7ef26c75dc1ca890ee20e6
Teke Byas <tekebayas@cis.vvc.edu>
2018-10-16 10:30:05 -0700

    Checks upper and lower - no predicate function

--- main.cpp
+++ main.cpp
@@ -10,16 +10,36 @@
 using namespace std;
 
 int a_in_string(string s);
+int upper_a(string s);
+int lower_a(string s);
+
 int main()
 {
     string s = "";
 
+    cout << endl; // white space
     cout << "Please input a string of A's (case does not matter):  ";
     cin >> s;
 
-    int count = a_in_string(s);
+    // sets each count
+    int count_all = a_in_string(s);
+    int count_upper = upper_a(s);
+    int count_lower = lower_a(s);
 
-    cout << "There are " << count << " A's in your string." << endl;
+    cout << endl; // white space
+
+    cout << "There are "
+         << count_upper
+         << " A's and "
+         << count_lower
+         << " a's in your string."
+         << endl
+         << "There are "
+         << count_all
+         << " A's in total."
+         << endl;
+
+    cout << endl; // white space
 
     return 0;
 }
@@ -35,6 +55,38 @@ int a_in_string(string s)
     for(int i = 0; i < s.length(); i++)
     {
         if(s.at(i) == 'a' || s.at(i) == 'A')
+        count++;
+    }
+    return count;
+}
+
+/*
+    Will only count A's
+    @param s the string the user entered
+    @return only how many A's there are
+*/
+int upper_a(string s)
+{
+    int count = 0;
+    for(int i = 0; i < s.length(); i++)
+    {
+        if(s.at(i) == 'A')
+        count++;
+    }
+    return count;
+}
+
+/*
+    Will only count a's
+    @param s the string the user entered
+    @return only how many a's there are
+*/
+int lower_a(string s)
+{
+    int count = 0;
+    for(int i = 0; i < s.length(); i++)
+    {
+        if(s.at(i) == 'a')
         count++;
     }
     return count;
